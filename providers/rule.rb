2380987aebf2a8d0995160234543a007c6a0eead
Dan Crosta <dcrosta@late.am>
2012-08-02 08:26:23 -0400

    test rules before adding them

--- providers/rule.rb
+++ providers/rule.rb
@@ -1,14 +1,21 @@
-action :append do
-  if not node["simple_iptables"]["chains"].include?(new_resource.chain)
-    node["simple_iptables"]["chains"] << new_resource.chain
-    node["simple_iptables"]["rules"] << "-A #{new_resource.direction} --jump #{new_resource.chain}"
-  end
+require 'chef/mixin/shell_out'
+include Chef::Mixin::ShellOut
 
+action :append do
   if new_resource.rule.kind_of?(String)
     rules = [new_resource.rule]
   else
     rules = new_resource.rule
   end
+
+  test_rules(new_resource, rules)
+
+  if not node["simple_iptables"]["chains"].include?(new_resource.chain)
+    node["simple_iptables"]["chains"] << new_resource.chain
+    node["simple_iptables"]["rules"] << "-A #{new_resource.direction} --jump #{new_resource.chain}"
+  end
+
+  # Then apply the rules to the node
   rules.each do |rule|
     new_rule = rule_string(new_resource, rule)
     if not node["simple_iptables"]["rules"].include?(new_rule)
@@ -21,8 +28,21 @@ action :append do
   end
 end
 
+def test_rules(new_resource, rules)
+  shell_out!("iptables --new-chain _chef_lwrp_test")
+  begin
+    rules.each do |rule|
+      new_rule = rule_string(new_resource, rule)
+      new_rule.gsub!("-A #{new_resource.chain}", "-A _chef_lwrp_test")
+      shell_out!("iptables #{new_rule}")
+    end
+  ensure
+    shell_out("iptables --flush _chef_lwrp_test")
+    shell_out("iptables --delete-chain _chef_lwrp_test")
+  end
+end
+
 def rule_string(new_resource, rule)
   rule = "-A #{new_resource.chain} #{rule} --jump #{new_resource.jump}"
   rule
 end
-
