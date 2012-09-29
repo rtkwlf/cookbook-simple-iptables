948eb9c391839f782e3a938a0241bb4b61530b26
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-09-29 22:04:21 +0800

    added NONE jump type for ignoring jump value

--- resources/rule.rb
+++ resources/rule.rb
@@ -2,7 +2,7 @@ actions :append
 
 attribute :chain, :name_attribute => true, :kind_of => String
 attribute :rule, :kind_of => [String, Array], :required => true
-attribute :jump, :equal_to => ["ACCEPT", "REJECT", "DROP"], :default => "ACCEPT"
+attribute :jump, :equal_to => ["ACCEPT", "REJECT", "DROP", "NONE"], :default => "ACCEPT"
 attribute :direction, :equal_to => ["INPUT", "FORWARD", "OUTPUT"], :default => "INPUT"
 
 
