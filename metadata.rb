ef313780e28dd31b8def04f28da44e3bbc63cbd4
Dan Crosta <dcrosta@late.am>
2013-02-13 07:41:57 -0500

    add `name` to metadata (you're welcome, foodcritic)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "BSD"
 description      "Simple LWRP and recipe for managing iptables rules"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.3"
+name             "simple_iptables"
 
 supports "debian", ">= 6.0"
 supports "redhat", ">= 5.8"
