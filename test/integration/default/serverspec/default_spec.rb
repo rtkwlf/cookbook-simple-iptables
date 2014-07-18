6e7ca7fb67bb117d0bf8443bdb9685792d5fd236
Pavel Yudin <pyudin@parallels.com>
2014-07-18 17:49:24 +0400

    Wrote some tests and configure test-kitchen.

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../../kitchen/data/spec_helper'
+
+describe iptables do
+  it { should have_rule('-A INPUT -j simple_rule') }
+  it { should have_rule('-A simple_rule -p tcp -m tcp --dport 80 -j ACCEPT') }
+  it { should have_rule('-A INPUT -p tcp -m tcp --dport 81 -j ACCEPT') }
+  it { should have_rule('-A FORWARD -p tcp -m tcp --dport 82 -j ACCEPT') }
+  it { should have_rule('-A INPUT -m state --state NEW -j jump_with_rule') }
+  it { should have_rule('-A jump_with_rule -p tcp -m tcp --dport 83 -j ACCEPT') }
+  it { should have_rule('-A array_of_rules -p tcp -m tcp --dport 84 -j ACCEPT') }
+  it { should have_rule('-A array_of_rules -p tcp -m tcp --dport 85 -j ACCEPT') }
+  it { should have_rule('-A INPUT -j array_of_rules') }
+end
