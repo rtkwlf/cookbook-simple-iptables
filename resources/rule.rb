59de27673ae469e1b511f87cd2090b0f0708e28e
Christoph Maser <christoph.maser@globalways.net>
2014-09-10 12:31:28 +0200

    Add comment attribute to simple_iptables_rule
    Add a string argument "comment" which is appendet as
     -m comment --comment "STRING"
    to a rule

--- resources/rule.rb
+++ resources/rule.rb
@@ -7,6 +7,7 @@ attribute :jump, :kind_of => [String, FalseClass], :default => "ACCEPT"
 attribute :direction, :equal_to => ["INPUT", "FORWARD", "OUTPUT", "PREROUTING", "POSTROUTING", :none], :default => "INPUT"
 attribute :chain_condition, :kind_of => [String]
 attribute :weight, :kind_of => Integer, :default => 50
+attribute :comment, :kind_of => String
 attribute :ip_version, :equal_to => [:ipv4, :ipv6, :both], :default => :ipv4
 
 def initialize(*args)
