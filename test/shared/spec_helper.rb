03e100d3f0f84c52202b3297f63e321bf1f1a07d
Larry Gilbert <larry@industrialtoys.com>
2014-12-22 17:25:56 -0800

    Update spec_helper for Serverspec 2.7.x
    Test Kitchen tests error out because spec_helper.rb was generated with
    an older version of Serverspec than Test Kitchen currently loads.

--- test/shared/spec_helper.rb
+++ test/shared/spec_helper.rb
@@ -1,12 +1,6 @@
 require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+
+set :backend, :exec
 
 # Require support files
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |file| require_relative(file) }
-
-RSpec.configure do |config|
-  config.before(:all) do
-    config.os = backend(Serverspec::Commands::Base).check_os
-  end
-end
