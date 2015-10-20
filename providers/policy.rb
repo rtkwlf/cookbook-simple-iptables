26dce70aa7df4458ececba0be6fe7250cbd5c323
chantra <chantr4@gmail.com>
2015-10-20 00:18:13 -0700

    Fix ipv6 support when using ip-version :both
    a ||= b is equivalent to a = a || b
    in ff467b78163d759e3b793ee812c2e1ef9b653744 support for ipv6 support got
    broken because updated was true after evaluating the ipv4 block.
    This caused handle_{policy,rule} to never be evaluated for ipv6.
    
    Fixes #75

--- providers/policy.rb
+++ providers/policy.rb
@@ -1,10 +1,10 @@
 action :set do
   updated = false
   if [:ipv4, :both].include?(new_resource.ip_version)
-    updated ||= handle_policy(new_resource, "ipv4")
+    updated |= handle_policy(new_resource, "ipv4")
   end
   if [:ipv6, :both].include?(new_resource.ip_version)
-    updated ||= handle_policy(new_resource, "ipv6")
+    updated |= handle_policy(new_resource, "ipv6")
   end
   new_resource.updated_by_last_action(updated)
 end
