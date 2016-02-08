5863d159b2d987b8203dc133b3af3275dc5ba54b
Andrew Heald <andrew@heald.co.uk>
2016-02-08 11:23:22 +0000

    1. Don't allow NAT rules on IPv6 on kernels older that 3.7. 2. Don't set up the IPv6 NAT table if no NAT rules are defined.

--- providers/rule.rb
+++ providers/rule.rb
@@ -8,6 +8,10 @@ action :append do
     updated |= handle_rule(new_resource, "ipv4")
   end
   if [:ipv6, :both].include?(new_resource.ip_version)
+    if new_resource.table == 'nat' &&
+        Gem::Version.new(/\d+(\.\d+(.\d+)?)?/.match(node['kernel']['release'])[0]) < Gem::Version.new('3.7')
+      raise "NAT table cannot be used with IPv6 before Kernel 3.7"
+    end
     updated |= handle_rule(new_resource, "ipv6")
   end
   new_resource.updated_by_last_action(updated)
